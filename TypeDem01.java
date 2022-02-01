18cae7ebc260e0073a27bfef68b55c578cfb069d
lxlone1913 <98717274+lxlone1913@users.noreply.github.com>
2022-02-01 22:34:44 +0800

    Add files via upload

---
+++ TypeDem01.java
@@ -0,0 +1,26 @@
+package com.exercisekeymap01.com.package01test;
+
+public class TypeDem01 {
+
+    public static void main(String[] args) {
+
+        byte a = 20;//byte,short,char,int,long,float,double
+        int b = a;
+        System.out.println(a);
+        System.out.println(b);
+
+        int age = 23;
+        double age1 = age;
+        System.out.println(age1);
+
+        char ch = 'a';
+        int code = ch;
+
+
+    }
+
+
+
+
+
+}
